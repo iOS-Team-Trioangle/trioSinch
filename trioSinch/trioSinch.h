08307c4b786a0c841806fe94b2085c1c3ebfd7e8
Rathna <rathnakumar11041994@gmail.com>
2024-03-25 12:25:42 +0530

    Initial Commit

---
+++ trioSinch/trioSinch.h
@@ -0,0 +1,18 @@
+//
+//  trioSinch.h
+//  trioSinch
+//
+//  Created by trioangle on 25/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for trioSinch.
+FOUNDATION_EXPORT double trioSinchVersionNumber;
+
+//! Project version string for trioSinch.
+FOUNDATION_EXPORT const unsigned char trioSinchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <trioSinch/PublicHeader.h>
+
+
